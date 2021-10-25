34ea340acfd6f51e57f7f07c02bf66b13ce21aa9
vish <vishgirase95@gmail.com>
2021-10-25 22:29:56 +0530

    [vishal] added REVERSE NUMBER DONE

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,74 +5,20 @@ namespace ConsoleApp1
 {
     class Program
     {
-      public static int Number = Convert.ToInt32(Console.ReadLine());
-     
-       public static  List<int> Divisor = new List<int>();
-        public static int sum=0;
-
-        public static void AddList()
-        {
-
-
-            for (int i = 1; i < Number; i++)
-            {
-
-                if (Number % i == 0)
-                {
-                    Divisor.Add(i);
-                   
-                }
-            }
-        }
-
-        //public static void PrintDivisor()
-        //{
-        //    for (int n = 0; n < Divisor.Count; n++)
-        //    {
-        //        Console.WriteLine(Divisor[n]);
-        //    }
-
-        //}
-
-        public static void addDivisor()
-        {
-            for (int m = 0; m < Divisor.Count; m++)
-            {
-                sum = Divisor[m] + sum;
-
-                   
-            }
-            
-        }
-
-        public static void Compare()
-        {
-            if (sum == Number)
-            {
-                Console.WriteLine($"{Number} is the Perfect Number");
-
-            }
-            else
-            {
-                Console.WriteLine($"{Number} is the not  Perfect Number");
-
-            }
-        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number to check if its Perfect:");
+            int Reverse = 0;
+            Console.WriteLine("Enter the number to be reverse:");
+            int Number = Convert.ToInt32(Console.ReadLine());
 
-            AddList();
-            //PrintDivisor();
-            addDivisor();
-            Compare();
-           
-
-           
-           
-
-
+            while(Number>0)
+            {
+                int LastDigit = Number % 10;
+                Reverse = (Reverse * 10) + LastDigit;
+                Number = Number / 10;
+            }
+            Console.WriteLine(Reverse);
 
 
 
